b13811df769c68f9aec85a5d7bbecffc5b3167ee
iwindy <203962638@qq.com>
2021-10-08 16:59:45 +0800

    2-9 集成热部署

--- src/main/java/com/iwindy/wiki/controller/TestController.java
+++ src/main/java/com/iwindy/wiki/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!!";
     }
 }
